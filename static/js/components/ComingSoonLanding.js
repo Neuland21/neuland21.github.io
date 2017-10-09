c8f64dd0754194be5635d0eb0e4c87b889f8b48d
ISABELLE <isabelle.k.miller@gmail.com>
2017-10-09 11:31:10 -0700

    styled fonts & added dark overlay to background

--- static/js/components/ComingSoonLanding.js
+++ static/js/components/ComingSoonLanding.js
@@ -11,7 +11,7 @@ const mailchimpFormProps = {
     btnLabel: 'Abonnieren',
     sending: 'Im Gange...',
     success: 'Abonniert!',
-    error: 'Irgendwas ist falsch gelaufen...'
+    error: 'Irgendwas ist falsch gelaufen.'
   },
   styles: {
     success: {
@@ -31,24 +31,26 @@ class ComingSoon extends Component {
 
   render() {
     return (
-      <div className='coming-soon-contents-container'>
+      <article className='overlay'>
+        <div className='coming-soon-contents-container'>
 
-        <div className='coming-soon-header'>Neuland 21</div>
-        <div className='coming-soon-subheader'>
-          Ein Think & Do Tank für das 
-          Landleben im 21. Jahrhundert
+          <div className='coming-soon-header'>Neuland 21</div>
+          <div className='coming-soon-subheader'>
+            Ein Think & Do Tank für das 
+            Landleben im 21. Jahrhundert
+          </div>
+          <div className='coming-soon-contents'>
+            Landflucht. Demographischer Wandel. Sterbende Dörfer. Die Herausforderungen für den ländlichen Raum sind nicht neu. Die politischen Lösungen allerdings auch nicht. Mit Neuland 21 holen wir das Landleben ins 21. Jahrhundert. 
+            <br /><br />
+            Als gemeinnütziger Think & Do Tank nutzen wir etablierte Methoden der Startup- und Kreativbranche, um die immer drängender werdenden Probleme unserer Dörfer und Kleinstädte anzugehen. Wir entwickeln und pilotieren digitale und soziale Innovationen in den Bereichen Nahversorgung, Mobilität, Gesundheit, Arbeit und Kultur.
+            <br /><br />
+            Und nicht vergessen: #ruralisthenewurban. 
+          </div>
+
+          { <MailchimpForm /> }
+
         </div>
-        <div className='coming-soon-contents'>
-          Landflucht. Demographischer Wandel. Sterbende Dörfer. Die Herausforderungen für den ländlichen Raum sind nicht neu. Die politischen Lösungen allerdings auch nicht. Mit Neuland 21 holen wir das Landleben ins 21. Jahrhundert. 
-          <br /><br />
-          Als gemeinnütziger Think & Do Tank nutzen wir etablierte Methoden der Startup- und Kreativbranche, um die immer drängender werdenden Probleme unserer Dörfer und Kleinstädte anzugehen. Wir entwickeln und pilotieren digitale und soziale Innovationen in den Bereichen Nahversorgung, Mobilität, Gesundheit, Arbeit und Kultur.
-          <br /><br />
-          Und nicht vergessen: #ruralisthenewurban. 
-        </div>
-
-        {<MailchimpForm />}
-
-      </div>
+      </article>
     )
   }
 }
